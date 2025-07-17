854dd44cd37338d8dbe24ab8c601e689a2d7aae7
Saakshi Thotad <saakshithotad15@gmail.com>
2025-07-17 16:05:15 +0530

    added styles.css and script.js files

---
+++ script.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const addTaskButton = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
+
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  tasks.forEach((task) => renderTask(task));
+
+  addTaskButton.addEventListener("click", function () {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") return;
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+    tasks.push(newTask);
+    saveTasks();
+    renderTask(newTask);
+    todoInput.value = "";
+  });
+
+  function renderTask(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed) li.classList.add("completed");
+    li.innerHTML = `
+            <span>${task.text}</span>
+            <button>delete</button>`;
+
+    li.addEventListener("click", (event) => {
+      if (event.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTasks();
+    });
+
+    li.querySelector("button").addEventListener("click", (event) => {
+      event.stopPropagation();
+      tasks = tasks.filter((t) => t.id !== task.id); //return all elements in the task array except for task with that particular id
+      li.remove();
+      saveTasks();
+    });
+
+    todoList.appendChild(li);
+  }
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+});
